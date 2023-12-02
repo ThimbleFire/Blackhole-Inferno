dcc5fca64248d5e2a81de1d84e522cc984efe4ce
Thimble <anthonyeboothroyd@gmail.com>
2023-12-02 19:51:46 +0000

    Update Station.cs

--- Blackhole Inferno/Assets/HUDStickers/Station.cs
+++ Blackhole Inferno/Assets/HUDStickers/Station.cs
@@ -4,6 +4,31 @@ using UnityEngine;
 
 public class Station : HUDSticker
 {
+    public class AttributeDistribution
+    {
+        private int[] attributes = new int[5];
+     
+        public AttributeDistribution()
+        {
+            RandomlyDistributePoints(100);
+        }
+
+        private void RandomlyDistributePoints(int totalPoints)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < attributes.Length - 1; i++)
+            {
+                int randomPoints = random.Next(0, totalPoints + 1);
+                attributes[i] = randomPoints;
+                totalPoints -= randomPoints;
+            }
+
+            // Assign the remaining points to the last attribute
+           attributes[attributes.Length - 1] = totalPoints;
+        }
+    }
+
     void Start()
     {
         signatureRadius = 65.0f;

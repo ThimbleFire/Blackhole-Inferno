687f3dfff10678836f84f106dce9e57a5cf1b42e
Thimble <lojith@proton.me>
2024-05-02 01:06:03 +0100

    Update HUDSticker.cs

--- Blackhole Inferno/Assets/Scripts/HUDStickers/HUDSticker.cs
+++ Blackhole Inferno/Assets/Scripts/HUDStickers/HUDSticker.cs
@@ -18,6 +18,10 @@ public class HUDSticker : MonoBehaviour, IPointerClickHandler, IPointerEnterHand
     public float signatureRadius;
     public bool globalVisibility = false;
 
+    // there's a bit of an oversight with these properties
+    // We're checking to see whether distance is within the cameras farClipPlane but the distance is based on the sticker and the ship, not the sticker and the camera.
+    // This is a problem because when we zoom out object should get further away, pulling them toward the center of the screen.
+    // We need to figure out when to check the distance between the worldPosition sticker and the ship and when to check the distance between the sticker and the camera.
     protected Vector3 direction { get { return (worldPosition - Ship.LPC.worldPosition).normalized; } }
     protected float distance { get { return Vector3.Distance(worldPosition, Ship.LPC.worldPosition); } }
     public Vector3 target3Position { get { return distance < Camera.main.farClipPlane ? worldPosition : direction * (Camera.main.farClipPlane - 10.0f); } }
